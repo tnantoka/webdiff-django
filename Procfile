eb8ee41e0095c28c2672d9cca2902cd140a7c718
tnantoka <tnantoka@bornneet.com>
2018-08-29 22:40:59 +0900

    Deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh -c 'cd ./webdiff && exec gunicorn webdiff.wsgi'
--- webdiff/webdiff/settings.py
+++ webdiff/webdiff/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i8zrs3k7#$01x+mj)2)h-%ay(w$ax0nat_xl4y4m(!(*9kejh6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
